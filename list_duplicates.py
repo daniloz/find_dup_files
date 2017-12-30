8d1a69af3f3e4dc9c20c615e5ccb8ab6808f6745
Danilo Zanatta <dan.zanatta@gmail.com>
2017-12-30 01:47:42 +0100

    ADD: convert unicode to utf-8 on list_duplicates CHG: removed unused imports

--- list_duplicates.py
+++ list_duplicates.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 from storage import *
 
-import sqlite3 as lite
-import sys, os
-import subprocess
-import string
-import md5
-import traceback
-
-
 def list_duplicates(size=1024*1024):
     db = Storage(memory = False, filename = "files.db")
     db.create_indices()
@@ -18,7 +10,7 @@ def list_duplicates(size=1024*1024):
         for r in db.files_by_crc32(crc):
             dr = dict(r)
             #print "%-80s (%10d)" % (dr['name'], dr['st_size'])
-            print "%s" % dr['name']
+            print "%s" % dr['name'].encode('utf-8')
         print '-'*100
 
 if __name__ == "__main__":
