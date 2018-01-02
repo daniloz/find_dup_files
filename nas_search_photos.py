03c39de1afbe2f0b54869054c7bc8d22bd1f86d5
Danilo Zanatta <dan.zanatta@gmail.com>
2018-01-02 08:46:33 +0100

    CHG: pass path as argument to nas_search_photos

--- nas_search_photos.py
+++ nas_search_photos.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-
 """
 Index files in subfolder
 """
 
+import sys
 from storage import Storage
 from search_storage import FindFiles
 
@@ -16,13 +16,20 @@ def print_duplicates(dbs):
 
 if __name__ == "__main__":
 
+    if len(sys.argv) < 2:
+        print "I need the path to work..."
+        sys.exit(1)
+    
+    path = sys.argv[1]
+    print "Searching for duplicate files in %s" % path
+
     DB2 = Storage(memory=False)
     DB2.recreate() # TODO
 
     __ITER__ = FindFiles(DB2)
 
     DB2.begin_adding_files()
-    PROBLEM_FILES = __ITER__.search('/volume1/photo/2009')
+    PROBLEM_FILES = __ITER__.search(path)
     DB2.done_adding_files()
 
     print "\nduplicate keys:"
